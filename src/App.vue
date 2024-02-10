c736c49d40c5a989b1d539882439785b05edd0da
Rafael Dos Santos <rafasennin@hotmail.com>
2024-02-10 11:01:41 -0300

    :tada: Iniciando o projeto

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app>
+    <v-main >
+      <div>
+        <v-card class="d-flex justify-center align-center">
+          <v-img src="./assets/rafasennin_logo.png" :max-width="500"  aspect-ratio="16/4"></v-img>
+        </v-card>
+
+        <h1 class="d-flex justify-center align-center ma-5">Pokedex</h1>
+        <v-responsive class="mx-auto" max-width="344">
+          <v-text-field label="Buscar Pokemon" placeholder="Digite o nome do pokemon" type="text" v-model="pokemonName"></v-text-field>
+          <v-btn color="success" class="mb-5" block @click="SearchPokemon">Buscar</v-btn>
+        </v-responsive>
+       
+      </div>
+      <v-card class="mx-auto" max-width="300">
+        <v-list>
+          <v-list-item v-for="(pokemon, index) in pokeListOrder" :key="pokemon">
+            <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index" />
+          </v-list-item>
+        </v-list>
+      </v-card>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+//Axios é uma das principais bibliotecas para fazer requisição http
+import axios from "axios";
+import Pokemon from "./components/Pokemon.vue"
+
+
+
+export default {
+  name: 'App',
+
+  components: {
+    Pokemon,
+  },
+
+  created() {
+
+    this.PokemonList();
+
+  },
+
+
+  methods: {
+    PokemonList() {
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0.").then(response => {
+        var unOrderList = response.data.results;
+        this.pokeListOrder = unOrderList;
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    SearchPokemon(){
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0.").then(response => {
+        var search = response.data.results;
+        if(this.pokeListOrder == "" || this.pokeListOrder == " "){
+          this.pokeListOrder = search
+        }else{
+        this.pokeListOrder = search.filter(pokemon => pokemon.name.toLowerCase() === this.pokemonName.toLowerCase());
+      }}).catch(error => {
+        console.log(error)
+      })
+    }
+    
+  },
+
+  data: () => ({
+
+    pokeListOrder: [],
+    pokemonName:""
+
+
+  }),
+}
+</script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-item class="bg-blue d-flex justify-center align-center text-center">
+        <v-card-title class="text-black">
+          <h1>{{ num + 1 }} {{ capitalizeFirstLetter(name) }}</h1>
+        </v-card-title>
+        <v-divider class="mt-5 "></v-divider>
+        <v-card-subtitle>
+          <p>{{ pokemon.type }}</p>
+        </v-card-subtitle>
+        <v-divider></v-divider>
+        <v-card-subtitle>
+          <v-img  :src="this.pokemon.isFront ? this.pokemon.front : this.pokemon.back"></v-img>
+        </v-card-subtitle>
+        <v-divider></v-divider>
+        <v-card-actions class="d-flex justify-center align-center text-center">
+          <v-btn class="bg-red" @click="turnPokemon">Turn</v-btn>
+        </v-card-actions>
+      </v-card-item>
+    </v-card>
+
+  </v-container>
+</template>
+  
+<script>
+import axios from "axios";
+
+ 
+
+export default {
+
+  created() {
+    axios.get(this.url).then(res => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+    })
+  },
+
+
+  data() {
+    return {
+      pokemon: {
+        type: '',
+        front: "",
+        back: '',
+        isFront: true,
+      },
+
+      tools: {
+        switchOn: true,
+        logo: "../assets/logo.png",
+        rafasennin: "@/assets/rafasennin_logo.png"
+      }
+
+    }
+  },
+
+  name: "Pokemon_component",
+
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+
+  methods: {
+    capitalizeFirstLetter(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+
+    },
+
+    turnPokemon() {
+      this.pokemon.isFront = !this.pokemon.isFront;
+    },
+  },
+};
+</script>
+  
